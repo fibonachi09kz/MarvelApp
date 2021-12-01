bcab2ef41e6f47aa1f346a763a07c34fcf8c2d82
Alexin Danila <danya09kz@gmail.com>
2021-12-02 01:25:38 +0600

    add lazy imports + singleComic page

---
+++ src/components/pages/404.js
@@ -0,0 +1,15 @@
+import ErrorMessage from "../errorMessage/ErrorMessage"
+import { Link } from "react-router-dom"
+
+
+const Page404 = () => {
+    return(
+        <div>
+            <ErrorMessage/>
+            <p style={{'text-align':'center', 'fontWeight':'bold', 'fontSize':'24px'}}>Page doesn't exist</p>
+            <Link exact to="/" style={{'display':'block', 'textAlign':'center','fontWeight':'bold', 'fontSize':'24px', 'marginTop':'30px'}}>Back to main page</Link>
+        </div>
+    )
+}
+
+export default Page404;
